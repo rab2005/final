0d8737502c92a171b48b3ed72d0e84a1ffe0abbe
rab2005 <153124649+rab2005@users.noreply.github.com>
2023-12-12 15:18:38 -0700

    Add files via upload

---
+++ js/myscripts.js
@@ -0,0 +1,7 @@
+function secretPage() {
+    if (prompt("What year did Rush release the album 'Moving Pictures'?")==1981){
+        window.location.href = "html/myhtml.html";
+    } else {
+        alert("You're a loser")
+    }
+    }
